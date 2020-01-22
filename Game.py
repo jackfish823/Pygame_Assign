d08c0506f2d8a92b9e7d6d3a449d15826aad5f55
Noam <46218256+jackfish823@users.noreply.github.com>
2020-01-22 07:21:36 +0200

    u

--- Game.py
+++ Game.py
@@ -1,8 +1,10 @@
 import pygame
 
 # Const
-WINDOW_WIDTH = 700
-WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 960
+WINDOW_HEIGHT = 540
+BACKGROUND_COLOR = (255,208,210)
+IMAGE = 'background1.jpg'
 
 # Init screen
 pygame.init()
@@ -10,10 +12,18 @@ size = (WINDOW_WIDTH, WINDOW_HEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Game")
 
+# Fill screen and show
+img = pygame.image.load(IMAGE)
+screen.blit(img, (0, 0))
+
+
+pygame.draw.circle(screen, BACKGROUND_COLOR, [480,270], 50) 
+pygame.display.flip()
+
 finish = False
 while not finish:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finish = True
-    
+
 pygame.quit()
